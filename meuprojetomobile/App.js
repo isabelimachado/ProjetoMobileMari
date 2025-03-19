5db3a942e38b607954aec9565079e67bb6d3dd1d
isa <isabelisantos0616@gmail.com>
2025-03-19 16:40:16 -0300

    Update App.js

--- meuprojetomobile/App.js
+++ meuprojetomobile/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
 
-
 export default function App() {
   return (
     <Login/>      
