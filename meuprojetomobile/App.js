a6798bd3d0d20b4abf8d4bac6169822b00821bb0
isa <isabelisantos0616@gmail.com>
2025-03-19 17:20:10 -0300

    commit

--- meuprojetomobile/App.js
+++ meuprojetomobile/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
