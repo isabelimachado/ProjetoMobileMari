9627ccd4b8ae9e5091913326063ba219ae9fdfca
isa <isabelisantos0616@gmail.com>
2025-04-23 17:28:35 -0300

    aaaaaaaaaaaaaaaaaaaaaa

--- meuprojetomobile/components/Card.js
+++ meuprojetomobile/components/Card.js
@@ -1,12 +1,13 @@
-import { View, Text, StyleSheet,FlatList,Image} from "react-native-web";
+import { View, Text, StyleSheet,Image} from "react-native";
 
 export default function card({nome,valor,img}){
     return(
         <View style={styles.card}>
-            <Image style={{height:300,width:300,borderRadius:10}} source={{uri:img}}></Image>
-            <Text style={styles.texto}>{nome}</Text>
-            <Text style={styles.texto}>R$ {valor}</Text>
-        </View>
+            <Image style={{height:200,width:200,borderRadius:10}} source={{uri:img}}></Image>
+            <View>
+                <Text style={styles.texto}>{nome}</Text>
+                <Text style={styles.texto}>R$ {valor}</Text></View>
+            </View>
     )
 }
 
@@ -17,17 +18,21 @@ const styles = StyleSheet.create({
         margin:10,
         backgroundColor:"#ef6f82",
         borderRadius: 8,
-        alignItems: "center",
         shadowColor: '#000',
         shadowOpacity: 0.2,
         shadowRadius: 50,
+        flexDirection: "row",
+        flex:1
+
+
     },
 
     texto:{
         fontFamily: 'Arial Narrow',
         fontSize:30,
         color: "black",
-        textAlign: 'center'
+        flex:1
+        
     },
 
 
