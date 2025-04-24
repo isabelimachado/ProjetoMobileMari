f691d4768b9c8ce8b2937e3b4cf82bcb245570b2
isa <isabelisantos0616@gmail.com>
2025-04-23 22:23:56 -0300

    arrumando os textos

--- meuprojetomobile/components/Card.js
+++ meuprojetomobile/components/Card.js
@@ -1,39 +1,40 @@
-import { View, Text, StyleSheet,Image} from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
-export default function card({nome,valor,img}){
-    return(
+export default function Card({ nome, valor, img }) {
+    return (
         <View style={styles.card}>
-            <Image style={{height:200,width:200,borderRadius:10}} source={{uri:img}}></Image>
+            <Image style={styles.imagem} source={{ uri: img }} />
             <View>
                 <Text style={styles.texto}>{nome}</Text>
-                <Text style={styles.texto}>R$ {valor}</Text></View>
+                <Text style={styles.texto}>R$ {valor}</Text>
             </View>
-    )
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-    
-    card:{
-        padding:10,
-        margin:10,
-        backgroundColor:"#ef6f82",
+    card: {
+        flexDirection: "row",
+        padding: 10,
+        margin: 10,
+        backgroundColor: "#ef6f82",
         borderRadius: 8,
         shadowColor: '#000',
         shadowOpacity: 0.2,
-        shadowRadius: 50,
-        flexDirection: "row",
-        flex:1
-
-
+        shadowRadius: 5,
+        alignItems: "center",
+    },
+    imagem: {
+        height: 100,
+        width: 100,
+        borderRadius: 10,
+        marginRight: 10,
     },
 
-    texto:{
-        fontFamily: 'Arial Narrow',
-        fontSize:30,
+    texto: {
+        fontFamily: 'Arial',
+        fontSize: 20,
         color: "black",
-        flex:1
-        
+        flexWrap: 'wrap',
     },
-
-
-})
+});
